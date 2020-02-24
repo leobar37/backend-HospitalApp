662f6699f70ee4b8184e92b099458cfd23b6986b
leobar37 <usatloqueando@gmail.com>
2020-02-24 01:52:47 -0500

    backend hospital copia

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '792650140448-ocikt7812icum1ighfel3jggfm9611i9.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'JqP-OOFHEhz9Otls154eaaxL';
